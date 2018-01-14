79751d1493a07905c257db4bc1799a8b104a5b42
emile baizel <emile@digit.co>
2018-01-13 22:17:21 -0800

    start adding the front end

--- web-client/src/components/cell.js
+++ web-client/src/components/cell.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+// import Request from '../request';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class Header extends Component {
+const contractTemplate = "abc";
+
+class Cell extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  createContract() {
+    const params = {
+      from: "0xbc84f3bf7dd607a37f9e5848a6333e6c188d926c",
+      value: 10, //
+      gas: 21000, //
+      data: contractTemplate, // the contract
+    }
+    var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
+    web3.eth.sendTransaction(params, function(err, transactionHash) {
+      console.log("got transactionHash ", transactionHash);
+      if (!err) {
+        web3.eth.getTransactionReceipt(transactionHash, function(err, receipt) {
+          console.log("Got receipt", receipt);
+        })
+      }
+    })
+  }
+
+  getContract(abi) {
+    var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
+    new web3.eth.Contract(abi).then(function(contract) {
+      console.log("got contract", contract)
+    }).catch(function(err) {
+      console.log("caught err", err)
+    })
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App-header">
-        <h3> Avalable Jobs! </h3>
+        <h3>Add New</h3>
+          <RaisedButton onClick={this.createContract} label="Create New"></RaisedButton>
       </div>
     );
   }
 }
 
-export default Header;
+
+
+export default Cell;
 
