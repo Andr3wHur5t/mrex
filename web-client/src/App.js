11145761ea34ed5d5c9f1d1fa998ca80f4b0674a
Andr3wHur5t <Andr3wHur5t@live.com>
2018-01-13 21:04:06 -0800

    Add more components to web app.

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import Header from './components/header'
+import Cell from './components/cell'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider>
+          <Header/>
+          <RaisedButton label="Default" />
+          <Cell/>
+          <Cell/>
+          <Cell/>
+          <Cell/>
+        </MuiThemeProvider>
       </div>
     );
   }
---
+++ web-client/src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h3> Avalable Jobs! </h3>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
